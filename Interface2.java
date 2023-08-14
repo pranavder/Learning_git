00fb370c0258fd6d70e23af0298582c94541acd8
PranavDeveloper <12345pranavbochare@gmail.com>
2023-08-14 09:58:21 +0530

    Interface

---
+++ Interface2.java
@@ -0,0 +1,18 @@
+interface Demo1{
+	static void m1(){
+		System.out.println("Demo-m1");
+	}
+}
+interface Demo2{
+	static void m1(){
+		System.out.println("DEmo-m11");
+	}
+}
+interface Demo3 extends Demo1,Demo2{
+}
+class Demochild implements Demo3{
+	public static void main(String[] args){
+		Demo1 obj=new Demochild();
+		obj.m1();
+	}
+}
